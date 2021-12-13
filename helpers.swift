e88df03fb2786ccb2bd8fc7ae9e37b5e9c73494d
Chris Williamson <chris.williamson@bbc.co.uk>
2021-12-13 09:23:38 +0000

    Matched helpers with 2020

--- helpers.swift
+++ helpers.swift
@@ -1,12 +1,12 @@
 import Foundation
 
 // READING INPUT
-func readInput<T: StringInitialisable>(fromTestFile: Bool) throws -> [T] {
+func readInput<T: StringInitialisable>(fromTestFile: Bool, separator: String = "\n") throws -> [T] {
     let inputPath = "./\(fromTestFile ? "test_" : "")input.txt"
 
     do {
         let contents = try String(contentsOfFile: inputPath, encoding: .utf8)
-        return Array(contents.split(separator: "\n")).map({ T(String($0))! })
+        return Array(contents.components(separatedBy: separator)).map({ T(String($0))! })
     }
     catch let error {
         print("Input parsing failed: \(error)")
@@ -27,11 +27,19 @@ extension ArraySlice where Element == Int {
     func sum() -> Int {
         self.reduce(0, +)
     }
+
+    func multiply() -> Int {
+        self.reduce(1, *)
+    }
 }
 
 extension Array where Element == Int {
     func sum() -> Int {
         self.reduce(0, +)
+    }
+
+    func multiply() -> Int {
+        self.reduce(1, *)
     }
 }
 
@@ -59,6 +67,10 @@ extension String {
         return self[i ..< i + 1]
     }
 
+    subscript (i: Int) -> Character {
+        return [Character](self[i ..< i + 1])[0]
+    }
+
     func substring(fromIndex: Int) -> String {
         return self[min(fromIndex, length) ..< length]
     }
@@ -76,7 +88,6 @@ extension String {
     }
 }
 
-// REGEX
 struct Regex {
     private let regex: NSRegularExpression
 
@@ -103,6 +114,26 @@ struct Regex {
             guard let substringRange = Range(matchRange, in: text) else { return nil }
             return String(text[substringRange])
         })
+    }
+}
+
+extension Array where Element: Equatable {
+    func combinations(count: Int) -> [[Element]] {
+        if count == 0 { return [[]] }
+
+        if count == 1 { return self.map({ [$0] }) }
+
+        let previousCombinations = combinations(count: count - 1)
+
+        var combinations: [[Element]] = []
+
+        for i in (0..<count) {
+            for j in (0..<previousCombinations.count) where !previousCombinations[j].contains(self[i]) {
+                combinations.append(previousCombinations[j] + [self[i]])
+            }
+        }
+
+        return combinations
     }
 }
 
